81be0db8f066d611b9879fc935f807a639dea0e5
Jason Lew <jason.a.lew@gmail.com>
2015-12-17 14:04:50 -0800

    Initial Commit

---
+++ JALAudioSampler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JALAudioSampler
+//
+//  Created by Jason Lew on 12/17/15.
+//  Copyright © 2015 Jason Lew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JALAudioSampler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JALAudioSampler
+//
+//  Created by Jason Lew on 12/17/15.
+//  Copyright © 2015 Jason Lew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
